b7a6573d1a870b688ab6b8e5b2469d942b873c39
ssraf <rafaelsanchezsouza@gmail.com>
2020-06-02 17:56:58 -0300

    produtos created

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProdutosComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
